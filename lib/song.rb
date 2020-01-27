0b2beecaa2728b92a35651b4fedbf24159700a3a
Justin Pierce <jwpierce1984@gmail.com>
2020-01-27 16:13:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,6 +26,7 @@ class Song
   def self.genre_count
     genre_count = {}
     genre_count.each do |genre|
-      if genre_count
+      if genre_count[genre]
+        genre_count[genre] >
   end
 end
