e2479ba1e48fc293f326392ff56f3e499ca07306
Justin Pierce <jwpierce1984@gmail.com>
2020-01-27 15:58:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   @@artists = []
   @@genres = []
   
-  attr_accessor :names, :artists, :genres
+  attr_accessor :name, :artist, :genre
   
   def initialize (name, artist, genre)
     @name << name
